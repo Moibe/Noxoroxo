5b875ef9f39f004897a2042c8a9d4d10886f12d1
Moibe <moi_estrello@hotmail.com>
2021-11-09 12:43:32 -0600

    Integración de Script
    Cambios para integrar el script para usar el contrato. Se dejará así porque ya se tiene una nueva solución en el siguiente commit.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const myInputOne = (e) => {
+state = "A";
+const lanzamiento; 
+
+function myInputOne(e) {
     document.getElementById('nameText').innerHTML = e.value;
 }
 
@@ -9,30 +12,40 @@ const myInputFour = (e) => {
     document.getElementById('numberText').innerHTML = e.value;
 }
 
+const printResult = async () => {
+    const a = await lanzamiento;
 
-const optionButton=(e)=>{
-if(e.classList.contains('optionOne')){
-    e.classList.remove('optionOne')
-    e.classList.add('optionTwo')
-    e.innerHTML=`Option B`;
-    formText.innerHTML=`My Form Version B`
-    body.classList.remove('gradient')
-    body.classList.add('gradient1')
-    // $('.growing_bar').css('background','red')
-    $('#adddedSuccessfull').css('display', 'none')
-    $('#barid').css('visibility', 'hidden')
-}
-else{
-    e.classList.remove('optionTwo');
-    e.classList.add('optionOne');
-    e.innerHTML=`Option A`;
-    formText.innerHTML=`My Form Version A`
-    body.classList.remove('gradient1')
-    body.classList.add('gradient')
-    $('#adddedSuccessfull').css('display', 'none')
-    $('#barid').css('visibility', 'hidden')
-    // $('.growing_bar').css('background','rgba(236, 0, 140, .6)')
-}
+    console.log("Esto se debería estar escribiendo hasta que tenemos un resultado de launch!!!:");
+    
+  };
+
+
+function optionButton(e) {
+    if (e.classList.contains('optionOne')) {
+        e.classList.remove('optionOne');
+        e.classList.add('optionTwo');
+        e.innerHTML = `Option B`;
+        formText.innerHTML = `My Form Version B`;
+        body.classList.remove('gradient');
+        body.classList.add('gradient1');
+        // $('.growing_bar').css('background','red')
+        $('#adddedSuccessfull').css('display', 'none');
+        $('#barid').css('visibility', 'hidden');
+        console.log("Sucedió Opción A");
+        state = "B";
+    }
+    else {
+        e.classList.remove('optionTwo');
+        e.classList.add('optionOne');
+        e.innerHTML = `Option A`;
+        formText.innerHTML = `My Form Version A`;
+        body.classList.remove('gradient1');
+        body.classList.add('gradient');
+        $('#adddedSuccessfull').css('display', 'none');
+        $('#barid').css('visibility', 'hidden');
+        // $('.growing_bar').css('background','rgba(236, 0, 140, .6)')
+        state = "A";
+    }
 }
 const myClick = (e) => {
 
@@ -57,9 +70,14 @@ const myClick = (e) => {
         }, 13000)
     }
     else{
-        alert(true)
-        
-        $('#barid').css('visibility', 'visible')
+        //alert(true)
+        console.log("En la siguiente línea llamamos a Launch()");
+        printResult();
+
+        lanzamiento = launch().then( () => {
+
+            console.log("Esto lo hace hasta que el launch se llevó a cabo.");
+            $('#barid').css('visibility', 'visible')
         $('.growing-bar').removeClass('growing_bar')
         // $('.growing-bar').css('animation-play-state','play')
         $('.growing-bar').addClass('growing_barB')
@@ -76,6 +94,10 @@ const myClick = (e) => {
             field3.value='';
     
         }, 13000)
+
+        });
+
+        
     }
     // if (nameOne.value >= '0' && (lastnameOne.value >= '0') && (numberOne.value >= '0')) {
         
--- launcher.js
+++ launcher.js
@@ -1,50 +1,70 @@
+//Elementos que usaremos para conectar Moralis. 
 const appId = "n3rg89swjLYr3w8QJApUcBJ8XpNlLPYdllWTFuqY"; 
 const serverUrl = "https://m02z2ncyupuz.bigmoralis.com:2053/server";
-const CONTRACT_ADDRESS_LAUNCH = "0x4ae8d2756ab677C909b539E981Df865277706D44";
 
-Moralis.start({ serverUrl, appId });
-
+//Elementos que usaremos para conectar la web3. 
 let web3; 
+const CONTRACT_ADDRESS_LAUNCH = "0x4ae8d2756ab677C909b539E981Df865277706D44"; //Está en Rinkeby, si lo usas en otra red te regresará vacio.
+
+//És necesario iniciar Moralis con start paara cualquier operación. 
+//Y todo lo que se hace aquí es con Moralis, no hay nada que se haga directo a la web3. 
+objeto = Moralis.start({ serverUrl, appId });
+console.log("Conectados a Moralis");
 
 async function init(){
-
+  //Primero revisa si hay un usuario de Moralis que se haya conectado previamente. 
     let currentUser = Moralis.User.current();
+    console.log(currentUser);
+    //Éste bloque es para poner alguna acción que quieras que haga al detectar que no hay un user de Moralis, 
+    //..previamente conectados.
     if(!currentUser){
         //windows.location.pathname = "/index.html";
-        console.log("Not user, por el momento no haré nada.");
-
-    }
-
+        console.log("No user logged yet.");
+    } else {
+    
+    
+    try{
     web3 = await Moralis.Web3.enable(); 
-    console.log("Conectados a Moralis");
+    console.log("Conectados a Web3:");
     console.log(web3);
+    console.log("Datos obtenidos de la cadena:")
     const chainIdHex = web3.currentProvider.chainId;
-        const chainIdDec = await web3.eth.getChainId();
-        console.log(chainIdHex);
-        console.log(chainIdDec);
-
-        try {
-            await web3.currentProvider.request({
-              method: "wallet_switchEthereumChain",
-              params: [{ chainId: "0x89" }]
-            });
-          } catch (error) {
-            alert(error.message);
-          }
-
-
-    let accounts = await web3.eth.getAccounts(); 
-     
-
+    const chainIdDec = await web3.eth.getChainId();
+    console.log(chainIdHex);
+    console.log(chainIdDec);
+  }
+  catch (error) {
+    //Esto saca un prompt alertando que no estás conectado via Metamask o lo que sea.      
+    //alert(error.message);
+    console.log(error);
+    console.log(error.message);
+    console.log("Como aún no estamos conectados, no hay más que hacer aquí por el momento.")
+       }
+      
+      }
+ 
 }
 
 async function launch(){
-    console.log("El botón SUMBMIT si sirvió!");
+
+  console.log("Launch se empezó a ejecutar");
+    //Lo primero que hago es obtener los valores que el usuario ya introdujo.
     //let name = document.getElementById("field1").value //Aquí iría un elemento que cachamos q varia según ambiente.
     let name = document.getElementById("field1").value
     let symbol = document.getElementById("field2").value
     //let amount = parseInt(document.getElementById("field3").value)
     let amount = document.getElementById("field3").value
+    
+    //Ahora si checa si está en la red correcta, por ahora la única red correcta será BNB Moralis.
+    try {
+            await web3.currentProvider.request({
+              method: "wallet_switchEthereumChain",
+              params: [{ chainId: "0x4" }]
+            });
+          } catch (error) {
+            alert(error.message);
+          }
+    
     const accounts = await web3.eth.getAccounts();
     const contract = new web3.eth.Contract(contractAbi, CONTRACT_ADDRESS_LAUNCH);
     console.log(contract);
@@ -54,6 +74,8 @@ async function launch(){
     console.log("Los valores si fueron obtenidos..");
     console.log(contract.methods);
     //Así estás llamando a la función int dentro del contrato en la blockchain, debes de poner los params que pide. 
+    //Va a pedir mucho en gas si el usuario no tiene BNB en la cuenta, en el futuro salta ésta parte para...
+    //.. advertirle antes al usuario de la falta de presuesto y que no parezca carísima de gas o ejecución por diseño.
     resultado = contract.methods.createToken(amount, name, symbol).send({from: accounts[0], value: Moralis.Units.ETH("0.001")})
     //contract.methods.payout().send({from: accounts[0], value: Moralis.Units.ETH("0")})
     .on("receipt", function(receipt)
@@ -81,7 +103,7 @@ async function launch(){
    
 }
 
-document.getElementById("submit_launch").onclick = launch;
+//document.getElementById("submit_launch").onclick = launch;
 
 init(); 
 
