18a758159d67af08975069e58cc9bcb74a54d424
Moibe <moi_estrello@hotmail.com>
2021-11-10 16:56:30 -0600

    Promise test

--- launcher.js
+++ launcher.js
@@ -17,52 +17,74 @@ console.log("Conectados a Moralis Launch y Crear Separados...");
 async function launch(){
 
   console.log("Entramos a launch...")
-  //Primero declaramos la promesa. 
-  let myPromise = new myPromise(function(myResolve, myReject) {
 
-  let x = 0;
-  
+  let myPromise = new Promise(function(myResolve, myReject) {
+    let x = 0;
+    console.log("Se imprimio el valor de x...")
   
   // The producing code (this may take some time)
-  console.log("Estoy ejecutando el login()...")
-  //Primero revisa si hay un usuario de Moralis que se haya conectado previamente. 
-    let currentUser = Moralis.User.current();
-    console.log("Usuario Actual:")
-    console.log(currentUser);
-    //Éste bloque es para poner alguna acción que quieras que haga al detectar que no hay un user de Moralis, 
-    //..previamente conectados.
-    if(!currentUser){
-        //windows.location.pathname = "/index.html";
-        console.log("Como no hay usuario abriré Metamask para que se conecte...");
-        try{
-          console.log("Me estoy conectando ahora a la Web3...")
-          web3 = Moralis.Web3.enable(); 
-          console.log("Conectados a Web3:");
-          console.log(web3);
-          console.log("Datos obtenidos de la cadena:")
-          const chainIdHex = web3.currentProvider.chainId;
-          const chainIdDec = web3.eth.getChainId();
-          console.log(chainIdHex);
-          console.log(chainIdDec);
-          console.log("Cambiaremos x a uno para indicar que nos logueamos.")
-          x = 1; 
-        }
-        catch (error) {
-          //Esto saca un prompt alertando que no estás conectado via Metamask o lo que sea.      
-          //alert(error.message);
-          console.log(error);
-          console.log(error.message);
-          console.log("Como aún no estamos conectados, no hay más que hacer aquí por el momento.")
-             }
-    }
   
-   
-    if (x == 1) {
-      myResolve("OK");
+    if (x == 0) {
+      myResolve(console.log("Resuelto"));
     } else {
-      myReject(error);
+      myReject(console.log("No resuelto"));
     }
   });
+
+  myPromise.then(
+    function(value) {console.log(value);},
+    function(error) {console.log(error);}
+  );
+  
+
+
+  
+  // //Primero declaramos la promesa. 
+  // let myPromise = new myPromise(function(myResolve, myReject) {
+
+  // let x = 0;
+  
+  
+  // // The producing code (this may take some time)
+  // console.log("Estoy ejecutando el login()...")
+  // //Primero revisa si hay un usuario de Moralis que se haya conectado previamente. 
+  //   let currentUser = Moralis.User.current();
+  //   console.log("Usuario Actual:")
+  //   console.log(currentUser);
+  //   //Éste bloque es para poner alguna acción que quieras que haga al detectar que no hay un user de Moralis, 
+  //   //..previamente conectados.
+  //   if(!currentUser){
+  //       //windows.location.pathname = "/index.html";
+  //       console.log("Como no hay usuario abriré Metamask para que se conecte...");
+  //       try{
+  //         console.log("Me estoy conectando ahora a la Web3...")
+  //         web3 = Moralis.Web3.enable(); 
+  //         console.log("Conectados a Web3:");
+  //         console.log(web3);
+  //         console.log("Datos obtenidos de la cadena:")
+  //         const chainIdHex = web3.currentProvider.chainId;
+  //         const chainIdDec = web3.eth.getChainId();
+  //         console.log(chainIdHex);
+  //         console.log(chainIdDec);
+  //         console.log("Cambiaremos x a uno para indicar que nos logueamos.")
+  //         x = 1; 
+  //       }
+  //       catch (error) {
+  //         //Esto saca un prompt alertando que no estás conectado via Metamask o lo que sea.      
+  //         //alert(error.message);
+  //         console.log(error);
+  //         console.log(error.message);
+  //         console.log("Como aún no estamos conectados, no hay más que hacer aquí por el momento.")
+  //            }
+  //   }
+  
+   
+  //   if (x == 1) {
+  //     myResolve("OK");
+  //   } else {
+  //     myReject(error);
+  //   }
+  // });
 
 }
 
