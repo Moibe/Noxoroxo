82a752f6874723f6d5dc315db9671868644e2ddb
Moibe <moi_estrello@hotmail.com>
2021-11-30 01:48:33 -0600

    Update launcher.js

--- launcher.js
+++ launcher.js
@@ -128,10 +128,12 @@ async function login(){
     
     resultado = await contract.methods.createToken(amount, name, symbol).send({from: accounts[0], value: Moralis.Units.ETH("0.001")})
     .on("transactionHash", (hash) => { console.log(hash) })
-    .on("receipt", (receipt) => {console.log(resultado) })
+    .on("receipt", (receipt) => {console.log(receipt) })
     .on("error", (error) => { console.log(error) })
     //.on("confirmation", (confirmationNumber, receipt) => { console.log(confirmationNumber) })
     
+    console.log("Esto es el resultado ahora si...")
+    console.log(resultado);
           
     
     // .on("receipt", function(receipt)
